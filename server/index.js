95349360ea57d74a3d9e2636f5520648a2169d88
Quang Linh Nguyễn <nquanglinh2604@gmail.com>
2020-09-21 17:04:31 +0700

    update

--- server/index.js
+++ server/index.js
@@ -14,13 +14,15 @@ const shop = require('./routers/api/shop');
 const user = require('./routers/api/user');
 const menu = require('./routers/api/menu');
 const item = require('./routers/api/item');
+const order = require('./routers/api/order');
 app.use('/api/course',course);
 app.use('/api/login',login);
 app.use('/api/shop',shop);
 app.use('/api/user',user);
 app.use('/api/menu',menu);
 app.use('/api/item',item);
+app.use('/api/order',order);
 
- const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
  app.listen(port, () => console.log(`Server started on post ${port}`));
---
+++ server/routers/api/order.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+
+router.post('/add',async (req,res) =>{
+    var user_id = req.body.user_id;
+    var course_id = req.body.course_id;
+    var db =  mysql.createConnection({
+        host        : 'localhost',
+        user        : 'root',
+        password    : '1234',
+        database    : 'foodorder'
+    });
+    await db.connect((error) => {
+        if(error){
+            throw error;
+        }
+    });
+    let sql = `INSERT INTO order (user_id,course_id) VALUES ('${user_id}','${course_id}')`;
+    await db.query(sql,(error) => {
+        if(error) throw error;
+        res.send('Success');
+    });    
+    db.end();
+});
+
+router.post('/detail',async (req,res) =>{
+    var order_id = req.body.order_id;
+    var item_id = req.body.item_id;
+    var quantity = req.body.quantity;
+    var total = req.body.total;
+    var db =  mysql.createConnection({
+        host        : 'localhost',
+        user        : 'root',
+        password    : '1234',
+        database    : 'foodorder'
+    });
+    await db.connect((error) => {
+        if(error){
+            throw error;
+        }
+    });
+    let sql = `INSERT INTO order (order_id,item_id,quantity,total) VALUES ('${order_id}','${item_id}','${quantity}','${total}')`;
+    await db.query(sql,(error) => {
+        if(error) throw error;
+        res.send('Success');
+    });    
+    db.end();
+});
+
+router.post('/', async(req,res) =>{
+    var user_id = req.body.user_id;
+    var db =  mysql.createConnection({
+        host        : 'localhost',
+        user        : 'root',
+        password    : '1234',
+        database    : 'foodorder'
+    });
+    await db.connect((error) => {
+        if(error){
+            throw error;
+        }
+    });
+    let sql = `SELECT orders.*,course.status,course.title,users.username FROM orders inner join course on course.id = orders.course_id inner join users on users.id = course.host_id having orders.user_id = '${user_id}'`;        
+    await db.query(sql,(error,result) => {
+        if(error) throw error;
+        
+        if(result.length > 0){
+
+            res.send(result);
+
+        }else res.status(401).json({error: "Error"});
+    });    
+    db.end();
+});
+router.post('/list', async(req,res) =>{
+    var user_id = req.body.user_id;
+    var course_id = req.body.course_id;
+    var db =  mysql.createConnection({
+        host        : 'localhost',
+        user        : 'root',
+        password    : '1234',
+        database    : 'foodorder'
+    });
+    await db.connect((error) => {
+        if(error){
+            throw error;
+        }
+    });
+    let sql = `SELECT orders.*,course.status,course.title,users.username FROM orders inner join course on course.id = orders.course_id inner join users on users.id = course.host_id having orders.user_id = '${user_id}' and orders.course_id = '${course_id}'`;        
+    await db.query(sql,(error,result) => {
+        if(error) throw error;
+        
+        if(result.length > 0){
+
+            res.send(result);
+
+        }else res.status(401).json({error: "Error"});
+    });    
+    db.end();
+});
+
+module.exports = router;
